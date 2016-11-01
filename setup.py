a0eae95f5660db8bb54dbb17ad7d0f4d7549825a
TAHRI Ahmed <nyuubi.10@gmail.com>
2016-11-01 22:56:24 +0100

    Ajout du script d'installation

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='pyAveryBerkel',
+    packages=['averyberkel'],
+    version='0.5.1',
+    description='Avery Berkel Scales management through TCP/IP',
+    author='Ahmed TAHRI',
+    author_email='ahmed@tahri.space',
+    url='https://github.com/Ousret/pyAveryBerkel',
+    download_url='https://github.com/Ousret/pyAveryBerkel/tarball/0.5.1',
+    keywords=['scale', 'averyberkel', 'mx200', 'mx100', 'mx400', 'mx'],
+    classifiers=[],
+)
